2378fd2d06d0edb1f4fa8173968152e43be234f3
ander <andermang@gmail.com>
2024-04-20 11:43:44 +0200

    Commit inicial

---
+++ archivo1.js
@@ -0,0 +1,3 @@
+<script>
+    console.log("La cabra");
+</script>
---
+++ archivo2.js
@@ -0,0 +1,8 @@
+<script>
+    const choco="5";
+    if(choco=289){
+        console.log("chocolate blanco")
+    }else{
+        console.log("Negritud")
+    }
+</script>
