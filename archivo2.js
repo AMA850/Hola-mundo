23d880e134da21985b0ad6f0daf2f99b0d113cbe
ander <andermang@gmail.com>
2024-04-20 11:46:56 +0200

    agregamos

--- archivo2.js
+++ archivo2.js
@@ -1,6 +1,7 @@
 <script>
-    const choco="5";
-    if(choco=289){
+    const choco=5;
+    let blanco=4;
+    if(choco===blanco){
         console.log("chocolate blanco")
     }else{
         console.log("Negritud")
